384b9d6b92f7cfd401b6918243c5cc298623db7e
Cliff <Ockiyacliffc@gmail.com>
2024-02-10 10:23:38 +0100

    Location

--- components/Location.tsx
+++ components/Location.tsx
@@ -47,7 +47,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
   )
 }
 
-const Camp = () => {
+const Location = () => {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
@@ -92,4 +92,4 @@ const Camp = () => {
   )
 }
 
-export default Camp
+export default Location
